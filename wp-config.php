a95019d5663e2997ee78c79fa97d69d7b5a84c3e
Omar Ruedy <theh2h@gmail.com>
2016-12-12 20:30:30 +0200

    added site url def in config.php

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,8 @@
  *
  * @package WordPress
  */
-
+// define('WP_HOME','http://localhost/omarfurrer/');
+// define('WP_SITEURL','http://localhost/omarfurrer/');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'portfolio');
